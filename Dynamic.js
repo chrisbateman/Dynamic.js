d6b5fe93e57dd5bbb1e491590abdf21833eeaec5
Chris Bateman <chrisb808@gmail.com>
2013-10-08 00:25:40 -0400

    Documentation; changed some variable names

--- Dynamic.js
+++ Dynamic.js
@@ -2,7 +2,7 @@
 window.Dynamic = (function() {
 	
 	var _models = [];
-	var _dynamicNodes = [];
+	var _dynamicElements = [];
 	var _submittableElements = [];
 	var _hideClass = 'dynamic-hide';
 	
@@ -11,16 +11,16 @@ window.Dynamic = (function() {
 	 * Gets all elements with specified attribute
 	 * Doesn't support 'for' attribute in ie7
 	 * @private
-	 * @param {String} node
+	 * @param {Element} el
 	 * @param {String} attr
 	 * @return {NodeList|Array} Matching nodes
 	 */
-	var _getElementsWithAttribute = function(node, attr) {
-		if (node.querySelectorAll) {
-			return node.querySelectorAll('[' + attr + ']');
+	var _getElementsWithAttribute = function(el, attr) {
+		if (el.querySelectorAll) {
+			return el.querySelectorAll('[' + attr + ']');
 		} else {
 			var matchingNodes = [];
-			var allNodes = node.getElementsByTagName('*');
+			var allNodes = el.getElementsByTagName('*');
 			
 			for (var i=0, iLen=allNodes.length; i<iLen; i++) {
 				var thisNode = allNodes[i];
@@ -36,15 +36,15 @@ window.Dynamic = (function() {
 	 * Add an event listener
 	 *
 	 * @private
-	 * @param {HTMLElement} node The node to add the listener to
+	 * @param {Element} el The element to add the listener to
 	 * @param {String} evt Name of the event to listen for
 	 * @param {Function} callback
 	 */
-	var _addEvent = function(node, evt, callback) {
-		if (node.addEventListener) {
-			node.addEventListener(evt, callback, false);
-		} else if (node.attachEvent)  {
-			node.attachEvent('on' + evt, callback);
+	var _addEvent = function(el, evt, callback) {
+		if (el.addEventListener) {
+			el.addEventListener(evt, callback, false);
+		} else if (el.attachEvent)  {
+			el.attachEvent('on' + evt, callback);
 		}
 	};
 	
@@ -56,7 +56,7 @@ window.Dynamic = (function() {
 	 * @Param {String} tag The type of element to listen for
 	 * @param {String} evt Name of the event to listen for
 	 * @param {Function} callback
-	 * @param {HTMLElement} container Node to add the listener to
+	 * @param {Element} container Element to add the listener to
 	 */
 	var _addDelegateByTag = function(tag, evt, callback, container) {
 		container = container || document.body;
@@ -82,21 +82,38 @@ window.Dynamic = (function() {
 		});
 	};
 	
-	var _isVisible = function(node) {
-		return node.offsetWidth > 0 && node.offsetHeight > 0;
+	
+	/**
+	 * Determines whether an element is currently visible
+	 * @param  {Element} el
+	 * @return {Boolean}
+	 */
+	var _isVisible = function(el) {
+		return el.offsetWidth > 0 && el.offsetHeight > 0;
 	};
 	
 	
-	var _getNodeValue = function(node) {
-		if (node.type === 'checkbox' || node.type === 'radio') {
-			return node.checked;
+	/**
+	 * Return's an input's current value
+	 * @param  {Element} el
+	 * @return {Boolean|String}
+	 */
+	var _getInputValue = function(el) {
+		if (el.type === 'checkbox' || el.type === 'radio') {
+			return el.checked;
 		}
-		return node.value;
+		return el.value;
 	};
 	
+	
+	/**
+	 * Returns the current value for a set of radios
+	 * @param  {Object} radioModel
+	 * @return {String}
+	 */
 	var _getRadioModelValue = function(radioModel) {
-		for (var i=0, iLen=radioModel.node.length; i<iLen; i++) {
-			var radio = radioModel.node[i];
+		for (var i=0, iLen=radioModel.element.length; i<iLen; i++) {
+			var radio = radioModel.element[i];
 			if (radio.checked) {
 				return radio.value;
 			}
@@ -104,27 +121,32 @@ window.Dynamic = (function() {
 		return '';
 	};
 	
-	var _initModel = function(node) {
-		if (node.nodeName === 'INPUT' || node.nodeName === 'SELECT') {
-			var modelName = node.getAttribute('data-model');
+	
+	/**
+	 * Creates a model for the given input and adds it to _models
+	 * @param  {Element} el
+	 */
+	var _initModel = function(el) {
+		if (el.nodeName === 'INPUT' || el.nodeName === 'SELECT') {
+			var modelName = el.getAttribute('data-model');
 			
-			if (node.type === 'radio') {
+			if (el.type === 'radio') {
 				if (_models[modelName]) {
-					_models[modelName].node.push(node);
+					_models[modelName].element.push(el);
 				} else {
 					_models[modelName] = {
-						node: [node],
+						element: [el],
 						value: ''
 					};
 				}
-				if (!node.getAttribute('name')) {
-					node.setAttribute('name', modelName);
+				if (!el.getAttribute('name')) {
+					el.setAttribute('name', modelName);
 				}
 				_models[modelName].value = _getRadioModelValue(_models[modelName]);
 			} else {
 				_models[modelName] = {
-					node: node,
-					value: _getNodeValue(node)
+					element: el,
+					value: _getInputValue(el)
 				};
 			}
 		} else {
@@ -133,6 +155,10 @@ window.Dynamic = (function() {
 	};
 	
 	
+	/**
+	 * Checks whether an input has changed - if so, run the rules
+	 * @this {Element} The input in question
+	 */
 	var _checkModel = function() {
 		var modelName = this.getAttribute('data-model');
 		
@@ -144,7 +170,7 @@ window.Dynamic = (function() {
 				if (this.type === 'radio') {
 					model.value = _getRadioModelValue(model);
 				} else {
-					model.value = _getNodeValue(this);
+					model.value = _getInputValue(this);
 				}
 				
 				if (model.value !== oldValue) {
@@ -156,32 +182,37 @@ window.Dynamic = (function() {
 		}
 	};
 	
+	
+	/**
+	 * Loops through each dynamic element and evaluates its rules against the current models and hides/shows
+	 */
 	var _applyRules = function() {
 		var flattenedModels = _getFlattenedModels();
-		var inputs = [];
 		
-		for (var i=0, iLen=_dynamicNodes.length; i<iLen; i++) {
-			var node = _dynamicNodes[i];
+		for (var i=0, iLen=_dynamicElements.length; i<iLen; i++) {
+			var el = _dynamicElements[i];
+			var expr = el.getAttribute('data-show');
 			var parsedExpressionValue;
-			var expr = node.getAttribute('data-show');
 			
 			with (flattenedModels) {
 				parsedExpressionValue = eval(expr); // it's either this or a large expression parsing library
 			}
 			
-			//node.style.display = (parsedExpressionValue) ? '' : 'none';
 			if (parsedExpressionValue) {
-				node.classList.remove(_hideClass);
+				el.classList.remove(_hideClass);
 			} else {
-				node.classList.add(_hideClass);
+				el.classList.add(_hideClass);
 			}
-			
 		}
 		
-		_checkSubmittableElements();
+		_setSubmittableElements();
 	};
 	
-	var _checkSubmittableElements = function() {
+	
+	/**
+	 * Loops through submittable elements and disables/enables based on visibility
+	 */
+	var _setSubmittableElements = function() {
 		for (var i=0, iLen=_submittableElements.length; i<iLen; i++) {
 			var input = _submittableElements[i];
 			if (_isVisible(input)) {
@@ -190,9 +221,13 @@ window.Dynamic = (function() {
 				input.setAttribute('disabled', 'disabled');
 			}
 		}
-	}
+	};
 	
 	
+	/**
+	 * Returns a flattened array of models (basically replaces each model with the model's value)
+	 * @return {Array}
+	 */
 	var _getFlattenedModels = function() {
 		var flattened = [];
 		for (var i in _models) {
@@ -202,33 +237,41 @@ window.Dynamic = (function() {
 	};
 	
 	
-	var _getSubmittableElements = function(node) {
+	/**
+	 * Retrives all inputs that might be submitted as part of a form, and adds to _submittableElements
+	 * @param  {Element} el The root element. If this is a submittable element, it will be added
+	 */
+	var _getSubmittableElements = function(el) {
 		var protoSlice = Array.prototype.slice;
 		
-		if (node.nodeName === 'INPUT' || node.nodeName === 'SELECT' || node.nodeName === 'TEXTAREA') {
-			_submittableElements.push(node);
+		if (el.nodeName === 'INPUT' || el.nodeName === 'SELECT' || el.nodeName === 'TEXTAREA') {
+			_submittableElements.push(el);
 		} else {
-			var inputs = protoSlice.call(node.getElementsByTagName('input'));
-			var selects = protoSlice.call(node.getElementsByTagName('select'));
-			var textareas = protoSlice.call(node.getElementsByTagName('textarea'));
+			var inputs = protoSlice.call(el.getElementsByTagName('input'));
+			var selects = protoSlice.call(el.getElementsByTagName('select'));
+			var textareas = protoSlice.call(el.getElementsByTagName('textarea'));
 			
 			_submittableElements = _submittableElements.concat(inputs).concat(selects).concat(textareas);
 		}
 	};
 	
 	
+	/**
+	 * Retrives and initializes all models and dynamic elements.
+	 * @public
+	 */
 	var _init = function() {
 		var modelList = _getElementsWithAttribute(document, 'data-model');
 		for (var i=0, iLen=modelList.length; i<iLen; i++) {
 			_initModel(modelList[i]);
 		}
 		
-		_dynamicNodes = [];
-		var dynNodes = _getElementsWithAttribute(document, 'data-show');
-		for (var i=0, iLen=dynNodes.length; i<iLen; i++) {
-			var node = dynNodes[i];
-			_dynamicNodes.push(node);
-			_getSubmittableElements(node);
+		_dynamicElements = [];
+		var dynEls = _getElementsWithAttribute(document, 'data-show');
+		for (var i=0, iLen=dynEls.length; i<iLen; i++) {
+			var el = dynEls[i];
+			_dynamicElements.push(el);
+			_getSubmittableElements(el);
 		}
 		
 		_applyRules();
@@ -249,7 +292,7 @@ window.Dynamic = (function() {
 	
 	
 	return {
-		_dynamicNodes: _dynamicNodes,
+		_dynamicElements: _dynamicElements,
 		_models: _models,
 		reinit: _init
 	};
